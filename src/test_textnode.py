fc23fef5dd9e815e80bd76907a8d23c1bc444f25
mlamb <h.max.lamb@gmail.com>
2024-07-11 19:04:23 -0400

    1.06: added more test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,17 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
 
+    def test_eq_url(self):
+        node3 = TextNode("This is a Node", "underlined", "http://website.xyz")
+        node4 = TextNode("This is a Node", "underlined", "http://website.xyz")
+        self.assertEqual(node3, node4)
+    
+    def test_eq_v2(self):
+        node5 = TextNode("This is a Node", "underlined", "")
+        node6 = TextNode("This is a Node", "underlined", "")
+        self.assertEqual(node5, node6)
+
+
 
 if __name__ == "__main__":
     unittest.main()

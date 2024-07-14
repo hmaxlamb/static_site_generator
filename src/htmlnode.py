fb116a572fddfee6b7166d6c684473fbf8a8679f
mlamb <h.max.lamb@gmail.com>
2024-07-14 19:54:41 -0400

    1.14: combines leaf and html node to the same file

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,6 +22,22 @@ class HTMLNode:
         return f"{self.tag}, {self.value}, {self.children}, {self.props}"
     
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None)
+        super.__init__(tag, value, None, props)
+        if self.value == None:
+            raise ValueError("No value for 'value' found")
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("No value for 'value' found")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
     
 
 

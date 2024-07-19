e186abcf9cb9e7a26606852bf87ed56569887649
mlamb <h.max.lamb@gmail.com>
2024-07-19 10:19:00 -0400

    1.19: finished the split_node function

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,15 +1,24 @@
 from textnode import (                                                                                                                                                                          
+    text_type_bold,
+    text_type_code,
+    text_type_image,
+    text_type_italic,
+    text_type_link,
+    text_type_text,
     TextNode
-    text_type_text
-    text_type_bold
-    text_type_italic
-    text_type_code
-    text_type_link
-    text_type_image
 )
 
 
 def split_node_delimiter(old_nodes, delimiter, text_type):
-    
-
-
+    new_node_list = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_node_list.append(node)
+        split_text = node.text.split(delimiter)
+        if split_text == node.text:
+            raise ValueError("Invalid delimiter")
+        new_node_list.extend([
+            TextNode(split_text[0], text_type_text)
+            TextNode(split_text[1], text_type)
+            TextNode(split_text[3], text_type_text)
+        ])

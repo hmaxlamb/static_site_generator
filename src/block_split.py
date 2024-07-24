2926d1da4d2d89c0ab72e7ff4c0bf46585424c24
mlamb <h.max.lamb@gmail.com>
2024-07-23 21:09:15 -0400

    1.41: fixed bugs in markdown to block func

--- src/block_split.py
+++ src/block_split.py
@@ -16,8 +16,10 @@ def markdown_to_blocks(markdown):
 
     for block in block_o_block_list:
         new_block = "\n".join(block)
+        if new_block == "":
+            continue
         return_list.append(new_block)
-        
+
     return return_list
 
 
--- src/test_mark_to_html.py
+++ src/test_mark_to_html.py
@@ -35,6 +35,47 @@ from mark_to_html import(
     markdown_to_html,
 )
 
-class TestMarktoHTML(unittest.TestCase):
-    def test(self):
-        pass
+class TestMarkdownToHTML(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
+        print(blocks)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                "* This is a list\n* with items",
+            ],
+        )
+    
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                "* This is a list\n* with items",
+            ],
+        )

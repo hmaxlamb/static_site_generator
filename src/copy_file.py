2ff16d830f653f668a7eb3192c66e2e545f41655
mlamb <h.max.lamb@gmail.com>
2024-07-25 20:28:55 -0400

    1.44: edited copy funtion

--- src/copy_file.py
+++ src/copy_file.py
@@ -1,7 +1,9 @@
 import os
 import shutil
 
-def map_files(dir, new_dir):
+def map_files(dir, new_dir, i = 1):
+    if i == 1:
+        shutil.rmtree(f"{new_dir}/*")
     dir_content = os.listdir(dir)
     for item in dir_content:
         if os.path.isfile(item):
